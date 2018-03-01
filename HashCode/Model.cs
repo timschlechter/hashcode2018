7255e3f2785f88e2f00702a0d2b4061ecf4930d4
Tim Schlechter <timschlechter@gmail.com>
2018-03-01 19:18:19 +0100

    Initial project setup

---
+++ HashCode/Model.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HashCode
+{
+    public class World
+    {
+        public int R { get; set; }
+        public int C { get; set; }
+        public int F { get; set; }
+        public int N { get; set; }
+        public int B { get; set; }
+        public int T { get; set; }
+    }
+
+    public class Ride
+    {
+        public int RideNumber { get; set; }
+
+        public int a { get; set; }
+        public int b { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public int s { get; set; }
+        public int f { get; set; }
+    }
+
+    public class Vehicle
+    {
+        public int M { get; set; }
+        public Ride[] R { get; set; }
+    }
+
+    
+}
